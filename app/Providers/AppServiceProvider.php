ed1e13643be4d7c6b132533aacf371c8fab5a6b3
Joel Camarillo <joelc33@gmail.com>
2021-09-05 07:14:56 -0400

    httpS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,11 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       if(env('REDIRECT_HTTPS'))
-        {
-            \URL::forceScheme('https');
-        }
-
-       
+              
     }
 }
