ab7b65edd9ef628a71b76107881659226ae7c8e9
Joel Camarillo <joelc33@gmail.com>
2021-09-05 07:55:38 -0400

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
-                URL::forceScheme('https');
-        //\URL::forceScheme('https');
+               
+        \URL::forceScheme('https');
         
     }
 }
