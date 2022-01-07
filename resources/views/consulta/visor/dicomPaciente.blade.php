088251a07e7925c10739c23b7972fca60cc40432
isilio <isilio16@gmail.com>
2022-01-07 15:30:46 -0400

    cambios en url dicom

--- resources/views/consulta/visor/dicomPaciente.blade.php
+++ resources/views/consulta/visor/dicomPaciente.blade.php
@@ -6,7 +6,7 @@
     title="Visor DICOM"
     width="100%"
     height="590"
-    src="https://teleconsulta1.gobel-a.com/webDICOM-paciente/index.html?id={{$archivo}}">
+    src="http://teleconsulta1.gobel-a.com/webDICOM-paciente/index.html?id={{$archivo}}">
 </iframe>
 
 
